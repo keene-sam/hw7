a43defa9706e3b1819bf245889d9623358c20979
Sammy Keene <sammy.keene@kellogg.northwestern.edu>
2021-05-17 13:02:37 -0500

    course review retrieval

--- netlify/functions/courses.js
+++ netlify/functions/courses.js
@@ -7,7 +7,7 @@
 // - Reviews can be written (anonymously) about the lecturer/course combination (what would that be called?)
 // - Reviews contain a String body, and a numeric rating from 1-5
 // - Keep it simple and ignore things like multiple course offerings and quarters; assume reviews are written
-//   about the lecturer/course combination only – also ignore the concept of a "user" and assume reviews
+//   about the lecturer/course combination only – also ignore the concept of a "user" and assume reviews
 //   are written anonymously
 //
 // Tasks:
@@ -44,7 +44,7 @@ exports.handler = async function(event) {
   let db = firebase.firestore()
 
   // ask Firebase for the course that corresponds to the course number, wait for the response
-  let courseQuery = await db.collection('courses').where(`courseNumber`, `==`, courseNumber).get()
+  let courseQuery = await db.collection(`courses`).where(`courseNumber`, `==`, courseNumber).get()
 
   // get the first document from the query
   let course = courseQuery.docs[0]
@@ -65,10 +65,16 @@ exports.handler = async function(event) {
   returnValue.sections = []
 
   // ask Firebase for the sections corresponding to the Document ID of the course, wait for the response
-  let sectionsQuery = await db.collection('sections').where(`courseId`, `==`, courseId).get()
+  let sectionsQuery = await db.collection(`sections`).where(`courseId`, `==`, courseId).get()
 
   // get the documents from the query
   let sections = sectionsQuery.docs
+
+  //create variable to store sum of all course ratings
+  let sumCourseRatings = 0
+
+  //create variable to store sum of number of course reviews
+  let countCourseReviews = 0
 
   // loop through the documents
   for (let i=0; i < sections.length; i++) {
@@ -82,11 +88,11 @@ exports.handler = async function(event) {
     let sectionObject = {}
 
     // ask Firebase for the lecturer with the ID provided by the section; hint: read "Retrieve One Document (when you know the Document ID)" in the reference
-    let lecturerQuery = await db.collection('lecturers').doc(sectionData.lecturerId).get()
+    let lecturerQuery = await db.collection(`lecturers`).doc(sectionData.lecturerId).get()
 
     // get the data from the returned document
     let lecturer = lecturerQuery.data()
-
+    
     // add the lecturer's name to the section Object
     sectionObject.lecturerName = lecturer.name
 
@@ -94,8 +100,60 @@ exports.handler = async function(event) {
     returnValue.sections.push(sectionObject)
 
     // 🔥 your code for the reviews/ratings goes here
-  }
+    let reviewsQuery = await db.collection('reviews').where(`sectionId`, `==`, sectionId).get()
 
+    //get review docs from the query
+    let reviews = reviewsQuery.docs
+    
+    //add the label "reviews" to the sectionObject with the data contained in reviewObject
+    sectionObject.reviews = []
+
+    //create a variable to hold sum of section ratings
+    let sumSectionRatings = 0
+
+    //loop through the documents
+    for(let j=0;j < reviews.length; j++){
+
+      // get the data from the review
+      let reviewData = reviews[j].data()
+      
+      // create an Object to be added to the return value of our lambda
+      let reviewObject = {}
+      
+      //add the review body to the reviewObject
+      reviewObject.body = reviewData.body
+
+      //add the review rating to the reviewObject
+      reviewObject.rating = reviewData.rating
+
+      //total the ratings for the section
+      sumSectionRatings += reviewObject.rating
+
+      //add the reviewObject to the sections
+      sectionObject.reviews.push(reviewObject)
+
+      //add average section rating value to the section object
+      sectionObject.averageSectionRating = sumSectionRatings/reviews.length
+
+      //add number of section reviews to the section object
+      sectionObject.totalSectionReviews = reviews.length
+
+    }
+
+    //store sum of all ratings across sections in the sum course rating variable
+    sumCourseRatings += sumSectionRatings
+
+    //store the number of reviews across sections in the count course reviews variable
+    countCourseReviews += reviews.length
+
+    //add the total course reviews to the returnValue object
+    returnValue.totalCourseReviews = countCourseReviews
+
+    //add the average course rating to the returnValue object
+    returnValue.averageCourseRating = sumCourseRatings/countCourseReviews
+     
+  }
+  
   // return the standard response
   return {
     statusCode: 200,
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyC_v4zvzONVucQkbUB_7EDaRYfYkEwi6fk",
+  authDomain: "kiei-451-869e3.firebaseapp.com",
+  projectId: "kiei-451-869e3",
+  storageBucket: "kiei-451-869e3.appspot.com",
+  messagingSenderId: "355009077528",
+  appId: "1:355009077528:web:e8f04aa75b9729667afe6d"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
